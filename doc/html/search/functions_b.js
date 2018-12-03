02511ae385129ac1753e72b4973c26173a6525cd
Carlos Palacios <carlosadrian.palaciosgonzalez@nxp.com>
2018-12-03 12:53:11 -0600

    updated documentation for LPSPI1 pins for S32K148

---
+++ doc/html/search/functions_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['wdog_5fdisable',['WDOG_disable',['../main_8c.html#ae8c3a22d3a6885669bf758d848dd0f49',1,'main.c']]],
+  ['wdog_5fewm_5firqhandler',['WDOG_EWM_IRQHandler',['../ewm_8c.html#a3412c1de21dec198a4c2f13ee4416157',1,'ewm.c']]]
+];
